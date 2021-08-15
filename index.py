f4dd82249f8d265f4d1cb304e18339512f4b1226
ArtakSukiasyan <armen93g@gmail.com>
2021-08-15 18:47:41 +0400

    added function calculate_frequencies() and /# Display your wordcloud image/ part of cod

--- index.py
+++ index.py
@@ -8,4 +8,32 @@ import sys
 
 #--------------------------------------
 file_contents=open('text.txt', 'r').read()
-#----------------------------
+#print(file_contents)
+
+#--------------------------------------
+
+
+#--------------------------------------
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a","in", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    
+    # LEARNER CODE START HERE
+   
+    
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(frequencies)
+    return cloud.to_array()
+#--------------------------------------
+# Display your wordcloud image
+
+#myimage = calculate_frequencies(file_contents)
+#plt.imshow(myimage, interpolation = 'nearest')
+#plt.axis('off')
+#plt.show()
